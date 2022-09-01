a5382f2480e859839f15e7d7c04cb939c7df5776
Felipe Sousa <felipe.sousa4030@gmail.com.br>
2022-08-31 22:47:59 -0300

    Add icons and Empty Activities

---
+++ app/src/main/java/com/example/pocusapp/ImportanceOfFocusActivity.java
@@ -0,0 +1,14 @@
+package com.example.pocusapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImportanceOfFocusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_importance_of_focus);
+    }
+}
---
+++ app/src/main/java/com/example/pocusapp/WhatIsPocusActivity.java
@@ -0,0 +1,14 @@
+package com.example.pocusapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WhatIsPocusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_what_is_pocus);
+    }
+}

18b55ae4800f8acf0242bdeb2cb639c3ce02ce51
Lauan Amorim <lauan12x@gmail.com>
2022-09-06 22:28:09 -0300

    buttons about working properly.

--- app/src/main/java/com/example/pocusapp/AboutActivity.java
+++ app/src/main/java/com/example/pocusapp/AboutActivity.java
@@ -2,7 +2,9 @@ package com.example.pocusapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+
+    public void chamarActivityPocus(View view) {
+        Intent intent = new Intent(this, WhatIsPocusActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void chamarActivityImportancia(View view) {
+        Intent intent = new Intent(this, ImportanceOfFocusActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void chamarActivityOquePomodoro(View view) {
+        Intent intent = new Intent(this, WhatIsPomodoroActivity.class);
+        startActivity(intent);
+        finish();
     }
 }

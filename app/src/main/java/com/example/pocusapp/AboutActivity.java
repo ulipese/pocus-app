b7a7f8e1d84c14a99c9175984bf0281e8906f37e
Lauan Amorim <lauan12x@gmail.com>
2022-09-19 20:55:03 -0300

    Main screen working properly (remaining functions)

--- app/src/main/java/com/example/pocusapp/AboutActivity.java
+++ app/src/main/java/com/example/pocusapp/AboutActivity.java
@@ -1,10 +1,16 @@
 package com.example.pocusapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -12,9 +18,36 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        } else {
+            ActivityCompat.requestPermissions(this,
+            new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        }
     }
 
-    public void chamarActivityPocus(View view) {
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
+
+    public void chamarActivityPocus (View view) {
         Intent intent = new Intent(this, WhatIsPocusActivity.class);
         startActivity(intent);
         finish();
@@ -31,4 +64,6 @@ public class AboutActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+
+
 }
--- app/src/main/java/com/example/pocusapp/MainActivity.java
+++ app/src/main/java/com/example/pocusapp/MainActivity.java
@@ -3,12 +3,103 @@ package com.example.pocusapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final long START_TIME_IN_MILLIS = 600000;
+
+    private TextView mTextViewCountDown;
+    private Button mButtonStartPause;
+    private android.widget.Button mButtonReset;
+
+    private CountDownTimer mCountDownTimer;
+
+    private boolean mTimerRunning;
+
+    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextViewCountDown = findViewById(R.id.text_view_timer);
+        mButtonStartPause = findViewById(R.id.btn_startPause);
+        mButtonReset = findViewById(R.id.btn_reset);
+    // setando o evento onClick
+        mButtonStartPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mTimerRunning) {
+                    pauseTimer();
+                } else {
+                    startTimer();
+                }
+            }
+        });
+    // setando o evento onClick
+        mButtonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetTimer();
+            }
+        });
+
+        updateCountDownText();
+
+    }
+
+    private void startTimer() {
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                mTimerRunning = false;
+                // muda o icon
+                mButtonStartPause.setBackgroundResource(R.drawable.ic_start_timer);
+                //muda a visibilidade do reset
+                mButtonReset.setVisibility(View.INVISIBLE);
+            }
+        }.start();
+        mTimerRunning = true;
+        // muda o icon
+        mButtonStartPause.setBackgroundResource(R.drawable.ic_stop_timer);
+        //Muda a visibilidade do reset
+        mButtonReset.setVisibility(View.INVISIBLE);
+    }
+
+    private void pauseTimer() {
+        mCountDownTimer.cancel();
+        mTimerRunning = false;
+        // muda o icon
+        mButtonStartPause.setBackgroundResource(R.drawable.ic_start_timer);
+        // Muda a visibilidade do reset
+        mButtonReset.setVisibility(View.VISIBLE);
+    }
+
+    private void resetTimer() {
+        mTimeLeftInMillis = START_TIME_IN_MILLIS;
+        updateCountDownText();
+        mButtonReset.setVisibility(View.INVISIBLE);
+    }
+
+    private void updateCountDownText() {
+        int minutes = (int) (mTimeLeftInMillis/1000) /60;
+        int seconds = (int) (mTimeLeftInMillis/1000) %60;
+
+        String timeLeftFormatted = String.format(Locale.getDefault(),"%02d:%02d", minutes, seconds);
+        mTextViewCountDown.setText(timeLeftFormatted);
     }
 }
